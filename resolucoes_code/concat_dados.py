5ac34adc1ec061e56028ae061903efdd4abba916
diegodev2006 <ds2365840@gmail.com>
2025-12-16 16:04:37 -0300

    meu primeiro commit

---
+++ resolucoes_code/concat_dados.py
@@ -0,0 +1,8 @@
+
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("Informação concatenada:", info_concatenada)
